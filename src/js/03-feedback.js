6341b1dd9e81162e25a5aaf144353dc56a0baf16
martadelka <oamedvedeva777@gmail.com>
2023-09-18 00:39:37 +0300

    js-hw8-task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
 
+const LOCAL_KEY = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(e) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    
+    const { email, message } = e.currentTarget.elements;
+    if (email.value === '' || message.value === '') {
+        alert('Please fill in all the fields!');
+        return;
+    }
+    const data = {
+        email: email.value,
+        message: message.value,
+    }
+    console.log(data)
+
+    localStorage.removeItem(LOCAL_KEY);
+    e.currentTarget.reset();
+    dataForm = {};
+}
