0ddc58c031f74fcdab815b308177233729962b4c
martadelka <oamedvedeva777@gmail.com>
2023-09-17 22:58:53 +0300

    js-hw8-task1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,36 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+const galleryFun = createImg(galleryItems);
+
+galleryList.insertAdjacentHTML("afterbegin", galleryFun);
+
+function createImg(galleryItems) {
+    const imgElem = galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li classs="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>    
+            </li>`;
+        })
+        .join('');
+    
+    return imgElem
+};
+
+var gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {
+    // do do
+});
+
